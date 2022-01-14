fdeb397b43d631e523079b6a4ef6dacd3ab1a4e9
Felix Leader <auradonfelix@gmail.com>
2022-01-14 17:27:47 +0100

    envoi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { WriteComponent } from './write/write.component';
+import { WindowComponent } from './window/window.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    WriteComponent,
+    WindowComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
